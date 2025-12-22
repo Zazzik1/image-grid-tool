0dff4aab7f8b2f9ddae2c9d495ad620a58603504
Mateusz Bilicki <mateuszbilicki34@gmail.com>
2025-12-22 16:08:01 +0100

    feat: added grid resize buttons that keep the same cell aspect ratio; test: added tests for the double and halve buttons (#15)

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -47,6 +47,54 @@ describe('page', () => {
         cy.updateNumberInputWithLabel('Line thickness', 10);
         cy.downloadAndMatchSnapshot('test-line-thickness-10.png');
     });
+    it('double grid button works as expected', () => {
+        cy.get('[data-test-name="double-grid"]').click();
+        cy.downloadAndMatchSnapshot('test-double-grid.png');
+    });
+    it('halve grid button works as expected', () => {
+        cy.get('[data-test-name="halve-grid"]').click();
+        cy.downloadAndMatchSnapshot('test-halve-grid.png');
+    });
+    it('increase grid button works as expected when cell aspect ratio is 1:1', () => {
+        cy.get('[data-test-name="cell-aspect-ratio"]').should(
+            'have.text',
+            '1:1',
+        );
+        cy.get('[data-test-name="increase-grid"]').click();
+        cy.downloadAndMatchSnapshot('test-increase-grid.png');
+    });
+    it('decrease grid button works as expected when cell aspect ratio is 1:1', () => {
+        cy.get('[data-test-name="cell-aspect-ratio"]').should(
+            'have.text',
+            '1:1',
+        );
+        cy.get('[data-test-name="decrease-grid"]').click();
+        cy.downloadAndMatchSnapshot('test-decrease-grid.png');
+    });
+    it('increase grid button does not change the cell aspect ratio when ratio is not 1:1', () => {
+        const ratioEl = cy.get('[data-test-name="cell-aspect-ratio"]');
+        const increaseButton = cy.get('[data-test-name="increase-grid"]');
+        const decreaseButton = cy.get('[data-test-name="decrease-grid"]');
+
+        cy.wait(200);
+
+        cy.updateNumberInputWithLabel('Number of rows', 5);
+        cy.updateNumberInputWithLabel('Number of columns', 7);
+
+        ratioEl.should('have.text', '10:21');
+
+        for (let i = 0; i < 10; i++) {
+            increaseButton.click();
+            cy.wait(100);
+            ratioEl.should('have.text', '10:21');
+        }
+
+        for (let i = 0; i < 10; i++) {
+            decreaseButton.click();
+            cy.wait(100);
+            ratioEl.should('have.text', '10:21');
+        }
+    });
 });
 
 // tests disabled in CI (antialiasing issue):
