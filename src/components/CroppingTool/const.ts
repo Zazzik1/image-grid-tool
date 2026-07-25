d4e120e43361d260d675bf5a98e9e6ced7f811fd
Zazzik1 <mateuszbilicki34@gmail.com>
2026-07-25 16:24:57 +0200

    moved contants to a separate file

--- src/components/CroppingTool/const.ts
+++ src/components/CroppingTool/const.ts
@@ -1,1 +1,61 @@
 export const CANVAS_HEIGHT = 600;
+
+export const availableRatios = [
+    {
+        value: 'free',
+        widthComponent: 1,
+        heightComponent: 1,
+        force: false,
+    },
+    {
+        value: '1:1',
+        widthComponent: 1,
+        heightComponent: 1,
+        force: true,
+    },
+    {
+        value: '3:2',
+        widthComponent: 3,
+        heightComponent: 2,
+        force: true,
+    },
+    {
+        value: '4:3',
+        widthComponent: 4,
+        heightComponent: 3,
+        force: true,
+    },
+    {
+        value: '5:4',
+        widthComponent: 5,
+        heightComponent: 4,
+        force: true,
+    },
+    {
+        value: '16:9',
+        widthComponent: 16,
+        heightComponent: 9,
+        force: true,
+    },
+    {
+        value: 'custom',
+        widthComponent: 1,
+        heightComponent: 1,
+        force: true,
+    },
+] as const;
+
+export type AvailableRatio = (typeof availableRatios)[number]['value'];
+
+export const orientations = [
+    {
+        value: 'horizontal',
+        title: 'Horizontal',
+    },
+    {
+        value: 'vertical',
+        title: 'Vertical',
+    },
+] as const;
+
+export type Orientation = (typeof orientations)[number]['value'];
--- src/components/CroppingTool/index.tsx
+++ src/components/CroppingTool/index.tsx
@@ -20,72 +20,18 @@ import {
     CropAreaHandles,
     CropAreaProps,
 } from './CropArea';
-import { CANVAS_HEIGHT } from './const';
+import {
+    AvailableRatio,
+    availableRatios,
+    CANVAS_HEIGHT,
+    Orientation,
+    orientations,
+} from './const';
 
 type Props = {
     image: HTMLImageElement;
     onSave: (image: HTMLImageElement) => void;
 };
-
-const availableRatios = [
-    {
-        value: 'free',
-        widthComponent: 1,
-        heightComponent: 1,
-        force: false,
-    },
-    {
-        value: '1:1',
-        widthComponent: 1,
-        heightComponent: 1,
-        force: true,
-    },
-    {
-        value: '3:2',
-        widthComponent: 3,
-        heightComponent: 2,
-        force: true,
-    },
-    {
-        value: '4:3',
-        widthComponent: 4,
-        heightComponent: 3,
-        force: true,
-    },
-    {
-        value: '5:4',
-        widthComponent: 5,
-        heightComponent: 4,
-        force: true,
-    },
-    {
-        value: '16:9',
-        widthComponent: 16,
-        heightComponent: 9,
-        force: true,
-    },
-    {
-        value: 'custom',
-        widthComponent: 1,
-        heightComponent: 1,
-        force: true,
-    },
-] as const;
-
-type AvailableRatio = (typeof availableRatios)[number]['value'];
-
-const orientations = [
-    {
-        value: 'horizontal',
-        title: 'Horizontal',
-    },
-    {
-        value: 'vertical',
-        title: 'Vertical',
-    },
-] as const;
-
-type Orientation = (typeof orientations)[number]['value'];
 
 const CroppingTool = ({ image, onSave }: Props) => {
     const bodyRef = useRef<HTMLDivElement>(null);
